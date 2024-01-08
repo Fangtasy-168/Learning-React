06c4c312c3ca89f17978f46fcb3ccb38e5e79d47
Tony Fang <tonyfang1897@gmail.com>
2024-01-07 20:11:05 -0500

    Learning React
    Following their Tic Tac Toe Tutorial on documentation

---
+++ Tic-Tac-Toe Tutorial/src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./styles.css";
+
+export default function Square() {
+  return (
+    <>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+    </>
+  )
+}
