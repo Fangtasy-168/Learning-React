19065ac4de180a7939ed04e7cc288db64647338b
Tony Fang <tonyfang1897@gmail.com>
2024-01-08 23:38:40 -0500

    Bringing State monitoring up to the parent
    moved the state tracking and the handleClick to the parent component Board() Modifed the handleClick function to accept i in order to change the respective squares clicked. Maintain immuntability by creating a copy and modifying the copy only to push back as the new state. Initialized squares with an array of 9 nulls. Modified the Square component to accept props of value, onSquareClick. added attribute of value and onSquareClick to the jSX calling of the square component. Note the onSquareClick attribute is an arrow function to not call the handleClick() on load as it would result in an endless loop of re-rendering

--- Tic-Tac-Toe Tutorial/src/App.js
+++ Tic-Tac-Toe Tutorial/src/App.js
@@ -1,44 +1,45 @@
 import React from "react"
 import { useState } from "react"
 
-
 // Component that creates a square allowing us to reuse as many times as needed in our code below
-function Square() {
-  const [value, setValue] = useState(null)
-  // value stores the value and setValue is a function that can be used to change the value.
-  // null is passed to useState to set inital value for the state variable. value starts off equal to null.
-
-  function handleClick() {
-    setValue('X')
-  }
-
+function Square({ value, onSquareClick }) {
   return (
     <button
       className="square"
-      onClick={handleClick}
-    >
+      onClick={onSquareClick}>
       {value}
     </button>
   )
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null)) // initialize squares to equal and array of 9 nulls
+
+  function handleClick(i) {
+    const nextSquares = squares.slice() // created a copy to modify to keep immuntability because when a parents state changes all children update
+    nextSquares[i] = 'X'
+    setSquares(nextSquares)
+  }
+
+
+  /* passing the squares state as a prop down to the component so it knows what to render. Also passing the function handleClick as a props */
   return (
     <>
       <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+
       </div>
       <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   )
