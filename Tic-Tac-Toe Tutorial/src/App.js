bf28c4efeada8e3e4d618d652ffd8595284012f7
Tony Fang <tonyfang1897@gmail.com>
2024-01-08 22:04:38 -0500

    interactive component
    changed the Square function instead of taking a value prop it starts monitoring state. To start monitoring state we imported useState from react added the const of value and setValue initialized value as null through useState(null). Used the setValue function in our handleClick to set value to 'X'. Removed the value attribute from the called components in Board component.

--- Tic-Tac-Toe Tutorial/src/App.js
+++ Tic-Tac-Toe Tutorial/src/App.js
@@ -1,28 +1,44 @@
 import React from "react"
+import { useState } from "react"
 
 
 // Component that creates a square allowing us to reuse as many times as needed in our code below
-function Square({ value }) {
-  return <button className="square">{value}</button>
+function Square() {
+  const [value, setValue] = useState(null)
+  // value stores the value and setValue is a function that can be used to change the value.
+  // null is passed to useState to set inital value for the state variable. value starts off equal to null.
+
+  function handleClick() {
+    setValue('X')
+  }
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  )
 }
 
 export default function Board() {
   return (
     <>
       <div className='board-row'>
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className='board-row'>
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className='board-row'>
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   )
