0c9bcefac24662d22c8674923589bf1447f124cb
Tony Fang <tonyfang1897@gmail.com>
2024-01-08 19:45:55 -0500

    updates

--- Tic-Tac-Toe Tutorial/src/App.js
+++ Tic-Tac-Toe Tutorial/src/App.js
@@ -1,11 +1,8 @@
 import React from "react"
-import "./styles.css";
 
 export default function Square() {
   return (
     <>
-      <button className="square">X</button>
-      <button className="square">X</button>
       <button className="square">X</button>
     </>
   )
